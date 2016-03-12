f067ea4a1e734b286adce03916e680a505a3e8c6
Deepak Sharma <sadysnaat@gmail.com>
2016-03-12 21:01:51 +0530

    Added gems chronic and colorize, updated Gemfile

--- lib/listable.rb
+++ lib/listable.rb
@@ -11,15 +11,15 @@ module Listable
     end
 
     if options.key? :due_date
-      formatted_date = options[:due_date].strftime("%D") if options[:due_date].is_a? Date
+      formatted_date = options[:due_date].strftime("%D") if options[:due_date].is_a? Time
       formatted_date = "No due date" if !formatted_date
     end
     return formatted_date
   end
   def format_priority(priority)
-    value = " ⇧" if priority == "high"
-    value = " ⇨" if priority == "medium"
-    value = " ⇩" if priority == "low"
+    value = " ⇧".colorize(:red) if priority == "high"
+    value = " ⇨".colorize(:yellow) if priority == "medium"
+    value = " ⇩".colorize(:green) if priority == "low"
     value = "" if !priority
     return value
   end
--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -1,12 +1,26 @@
 class UdaciList
   attr_reader :title, :items
 
+  @@untitled_lists = 0
+  @@valid_types = ["todo","event","link"]
+
   def initialize(options={})
-    @title = options[:title]
+    if options[:title]
+      @title = options[:title]
+    else
+      @title = "Untitled List"
+      if @@untitled_lists > 0
+        @title += @@untitled_lists.to_s
+        @@untitled_lists += 1
+      end
+    end
     @items = []
   end
   def add(type, description, options={})
     type = type.downcase
+    if !@@valid_types.include? type
+      raise UdaciListErrors::InvalidItemType, "#{type} is not a valid Udacilist Item"
+    end
     @items.push TodoItem.new(description, options) if type == "todo"
     @items.push EventItem.new(description, options) if type == "event"
     @items.push LinkItem.new(description, options) if type == "link"
